91ff0458f4e6972b70771b751e7a3d0391150aec
maandeep98 <maandeep998@gmail.com>
2018-08-24 19:21:57 +0530

    push pop linked list

---
+++ linkedList.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+struct node *head;
+
+struct node* createNode(int d) {
+    struct node *nn = malloc(sizeof(struct node));
+    nn->data = d;
+    nn->next = NULL;
+    return nn;
+}
+
+void push(int d) {
+    struct node *f = createNode(d);
+    f->next = head;
+    head = f;
+}
+
+void pop() {
+    int *ptr = head->next;
+    free(head);
+    head = ptr;
+}
+
+int main() {
+    push(5);
+    push(10);
+    struct node *curr;
+    curr = head;
+    while(curr != NULL) {
+        printf("%d\n",curr->data);
+        curr = curr->next;
+    }
+}
