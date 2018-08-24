24b808f3bdfe1bc4510feb59cb6af0cf3ee439ae
maandeep98 <maandeep998@gmail.com>
2018-08-24 19:55:16 +0530

    linked list header file

---
+++ linkedList.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+struct node *head;
+
+struct node* createNode(int d) {
+    struct node *nn = malloc(sizeof(struct node));
+    nn->data = d;
+    nn->next = NULL;
+    return nn;
+}
+
+void push(int d) {
+    struct node *f = createNode(d);
+    f->next = head;
+    head = f;
+}
+
+void pop() {
+    int *ptr = head->next;
+    free(head);
+    head = ptr;
+}
+
+int checkEmpty() { return !!head; }
+
+int getFirstNode() { return (head->data); }
